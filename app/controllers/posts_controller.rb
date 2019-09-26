2c9b9c58ac83092f4b40eebe36062cb857d97ea3
Justin Maple <justinamaple@gmail.com>
2019-09-26 00:02:42 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,8 +9,10 @@ class PostsController < ApplicationController
 
   def update
     if @post.update(post_params)
-
-    redirect_to post_path(@post)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
